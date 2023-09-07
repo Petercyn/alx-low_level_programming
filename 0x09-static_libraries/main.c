1faec7112098ae9c6c602d1bf4d9f65b137fef1e
Petercyn <cynthianapeter03@gmail.com>
2023-09-06 19:31:08 -0700

    main.c file

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main(void)
+{
+    _puts("\"To know and not to do is really not to know, I am yet to become a great programmer\"\n\t- The great power of yet");
+    return (0);
+}
